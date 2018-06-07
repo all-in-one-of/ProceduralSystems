d40771fd778665f4e6e0d58737c4a985ef805a4b
Nathan Koch <nathanrkoch@gmail.com>
2018-06-07 18:02:49 -0400

    initial commit

---
+++ Sketches/Week01/Sketch01/TileSketch01.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TileSketch01" tilewidth="256" tileheight="256" tilecount="16" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 7.png"/>
+ </tile>
+ <tile id="7">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 8.png"/>
+ </tile>
+ <tile id="8">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 9.png"/>
+ </tile>
+ <tile id="9">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 12.png"/>
+ </tile>
+ <tile id="12">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 13.png"/>
+ </tile>
+ <tile id="13">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 14.png"/>
+ </tile>
+ <tile id="14">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 15.png"/>
+ </tile>
+ <tile id="15">
+  <image width="256" height="256" source="../Dropbox/MFADT/Summer 2018/Procedural Systems/Sketches/TileSketch01/Tile 16.png"/>
+ </tile>
+</tileset>

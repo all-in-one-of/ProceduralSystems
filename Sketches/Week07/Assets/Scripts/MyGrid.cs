6516ee20ee48a089ba52ec5ff6ba398a4baab793
Nathan Koch <nathanrkoch@gmail.com>
2018-09-05 15:17:44 -0400

    week 7 project started

---
+++ Sketches/Week07/Assets/Scripts/MyGrid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class MyGrid : MonoBehaviour
+{
+    public int xSize, ySize;
+    public GameObject point;
+    private Vector3[] vertices;
+
+    public void Awake()
+    {
+        StartCoroutine("Generate");
+    }
+
+    private IEnumerator Generate()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.1f);
+
+        int arraySize = (xSize + 1) * (ySize + 1);
+        vertices = new Vector3[arraySize];
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+                Instantiate(point, vertices[i], Quaternion.identity);
+                yield return wait;
+            }
+        }
+    }
+}
---
+++ Sketches/Week07/Assets/Scripts/Sketch04MyGrid.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Sketch04MyGrid : MonoBehaviour
+{
+    public int xSize, ySize;
+    private Vector3[] vertices;
+    private Mesh mesh;
+    public float heightScale = 1.0f;
+    public float speed = 0.01f;
+    private float noiseTime = 0f;
+    public int turnaround = 120;
+
+    public void Awake()
+    {
+        StartCoroutine("Generate");
+    }
+
+    private void Generate()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
+        int arraySize = (xSize + 1) * (ySize + 1);
+        vertices = new Vector3[arraySize];
+        Vector2[] uv = new Vector2[vertices.Length];
+        Vector4[] tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                tangents[i] = tangent;
+            }
+        }
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+
+        int[] triangles = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+            }
+        }
+
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    private void Update()
+    {
+        int arraySize = (xSize + 1) * (ySize + 1);
+        vertices = new Vector3[arraySize];
+        Vector2[] uv = new Vector2[vertices.Length];
+        Vector4[] tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y, heightScale * Mathf.PerlinNoise(noiseTime + x, 0f));
+                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                tangents[i] = tangent;
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.RecalculateNormals();
+
+        if (Time.frameCount > (turnaround * 2))
+        {
+            noiseTime = 0;
+        }
+
+        if (Time.frameCount >= turnaround && speed > 0) speed *= -1;
+
+        noiseTime += speed;
+
+    }
+
+    public float scale(float OldMin, float OldMax, float NewMin, float NewMax, float OldValue)
+    {
+
+        float OldRange = (OldMax - OldMin);
+        float NewRange = (NewMax - NewMin);
+        float NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin;
+
+        return (NewValue);
+    }
+}

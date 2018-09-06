b42c2432cba2a9c7e171837af001d7afbca89589
Nathan Koch <nathanrkoch@gmail.com>
2018-09-06 11:25:56 -0400

    new unity recorder

---
+++ Sketches/Week07/Assets/Scripts/Sketch01/TransformCube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformCube : MonoBehaviour
+{
+    private Mesh mesh;
+
+    // Use this for initialization
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3[] vertices = mesh.vertices;
+        Vector3[] normals = mesh.normals;
+
+        vertices[0] += new Vector3(0, 0.01f, 0);
+        vertices[1] += new Vector3(0, 0.01f, 0);
+
+        mesh.vertices = vertices;
+    }
+}

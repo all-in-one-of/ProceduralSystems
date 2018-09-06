737aa233f3a0d097897893ff415588c17b48b14e
Nathan Koch <nathanrkoch@gmail.com>
2018-09-06 12:39:55 -0400

    sketch 3

--- Sketches/Week07/Assets/Scripts/Sketch03/NoiseOneSideTransform.cs
+++ Sketches/Week07/Assets/Scripts/Sketch03/NoiseOneSideTransform.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TransformCubeExpand : MonoBehaviour
+public class NoiseOneSideTransform : MonoBehaviour
 {
+    public float noiseScale = 0.1f;
     private Mesh mesh;
     private List<Vertex> oneSide = new List<Vertex>();
+    private List<Vertex> topOfOneSide = new List<Vertex>();
 
     void Start()
     {
@@ -18,22 +20,34 @@ public class TransformCubeExpand : MonoBehaviour
             {
                 var v = new Vertex(vertices[i], i);
                 oneSide.Add(v);
+                if (vertices[i].y == 0.5)
+                {
+                    topOfOneSide.Add(v);
+                }
             }
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3[] vertices = mesh.vertices;
         Vector3[] normals = mesh.normals;
+        float displaceAmount = (0.5f - Mathf.PerlinNoise(Time.time, 0.0f)) * noiseScale;
 
         for (int i = 0; i < oneSide.Count; i++)
         {
             var vertex = oneSide[i];
-            vertex.position = vertex.position + new Vector3(0.01f, 0, 0);
+            vertex.position += new Vector3(displaceAmount, 0, 0);
             vertices[vertex.index] = vertex.position;
             oneSide[i] = vertex;
+        }
+
+        for (int i = 0; i < topOfOneSide.Count; i++)
+        {
+            var vertex = topOfOneSide[i];
+            vertex.position += new Vector3(0, displaceAmount, 0);
+            vertices[vertex.index] = vertex.position;
+            topOfOneSide[i] = vertex;
         }
 
         mesh.vertices = vertices;
---
+++ Sketches/Week07/Assets/Scripts/Vertex.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vertex
+{
+    public int index;
+    public Vector3 position;
+
+    public Vertex(Vector3 newPosition, int newIndex)
+    {
+        position = newPosition;
+        index = newIndex;
+    }
+}
